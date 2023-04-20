f004745a2b1d22fc36b938106f8725a2dcffd432
ayakayal <aya.kayal.21@ucl.ac.uk>
2023-04-20 13:21:19 +0100

    backup for curiosity models

---
+++ curiosity_models.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+
+
+# Function from https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/model.py
+def init_params(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        m.weight.data.normal_(0, 1)
+        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+def init(module, weight_init, bias_init, gain=1):
+    weight_init(module.weight.data, gain=gain)
+    bias_init(module.bias.data)
+    return module
+class MinigridStateEmbeddingNet(nn.Module):
+
+    def __init__(self, obs_space):
+        super().__init__()
+        #image embedding part of the network (I got it from ModelAC above)
+        self.image_conv = nn.Sequential(
+            nn.Conv2d(3, 16, (2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(16, 32, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (2, 2)),
+            nn.ReLU()
+        )
+        obs_space = {"image": obs_space.spaces["image"].shape, "text": 100}
+        n = obs_space["image"][0]
+        m = obs_space["image"][1]
+        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+        self.apply(init_params)
+
+    def forward(self, obs):
+        x = obs.image.transpose(1, 3).transpose(2, 3)
+        x = self.image_conv(x)
+        x = x.reshape(x.shape[0], -1)
+        # -- [unroll_length x batch_size x height x width x channels]
+        return x
+    
+class MinigridForwardDynamicsNet(nn.Module):
+    def __init__(self, image_embedding_size, action_space):
+        super().__init__()
+        self.num_actions= action_space.n
+        self.image_embedding_size=image_embedding_size
+        #i got init_ function from RIDE
+        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
+                        constant_(x, 0), nn.init.calculate_gain('relu'))
+        #image embedding part of the network (I got it from ModelAC above)
+       
+       
+
+        #this part from RIDE
+        self.forward_dynamics = nn.Sequential(
+            init_(nn.Linear(64 + self.num_actions, 256)), 
+            nn.ReLU(), 
+        )
+
+        init_ = lambda m: init(m, nn.init.orthogonal_, 
+            lambda x: nn.init.constant_(x, 0))
+
+        self.fd_out = init_(nn.Linear(256,64))
+
+    def forward(self, obs, action):
+
+        #this part from my code
+        #print('action',action)
+        #print('length',len(action))
+        one_hot = torch.zeros(len(action),self.num_actions) #,device='cuda:5'
+        for i in range(len(action)):
+
+            #print('hey',action[i])
+            action_tensor=torch.tensor([action[i].item()], dtype=torch.int32).long() #, device='cuda:5'
+            #print('action tensor',action_tensor)
+            one_hot[i] = torch.nn.functional.one_hot(action_tensor, num_classes=self.num_actions)
+            #one_hot[i]= F.one_hot(action[i], num_classes=self.num_actions).float()
+       
+        #print('one hot',one_hot.shape)
+        #one_hot.to('cuda:5')
+        #action_one_hot = F.one_hot(action, num_classes=self.num_actions).float()
+        #print('obs tens',obs.shape)
+        #print('one hot shape',one_hot)
+        inputs = torch.cat((obs, one_hot), dim=1)
+        #print('inputs',inputs.shape)
+        
+        next_state_emb = self.fd_out(self.forward_dynamics(inputs))
+        return next_state_emb
